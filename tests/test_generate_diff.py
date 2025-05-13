bc536587e4797f7bb9c0c59bf42180df4adc0bef
volkbav <berezov86@gmail.com>
2025-05-13 21:59:34 +0300

    add tests for step 3

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,14 @@
+from gendiff import generate_diff
+
+def test_step3():
+    first_file_content, second_file_content = generate_diff(
+        'tests/fixtures/file1.json',
+        'tests/fixtures/file2.json'
+        )
+    print(
+        f'\n---test_step3---\n'
+        f'file1: \n {first_file_content}\n',
+        f'file2: \n {second_file_content}\n',
+        f'---test_step3---\n'
+          )
+
