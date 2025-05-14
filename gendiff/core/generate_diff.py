d0105da395c87fc7ffde77da75634709ed51db1c
volkbav <berezov86@gmail.com>
2025-05-14 17:13:04 +0300

    step4

--- gendiff/core/generate_diff.py
+++ gendiff/core/generate_diff.py
@@ -3,9 +3,9 @@ from gendiff.core.parse import parse_json
 def generate_diff(file_path1, file_path2, format=None):
     first_file_content = parse_json(file_path1)
     second_file_content = parse_json(file_path2)
-    diff = make_diff(first_file_content, second_file_content)
-    
-    return first_file_content, second_file_content, diff
+    diff_tree = make_diff(first_file_content, second_file_content)
+    result = flat(diff_tree)
+    return result
 
 
 def make_diff(file_data1, file_data2):
@@ -38,3 +38,27 @@ def make_diff(file_data1, file_data2):
             }
     return diff
             
+def flat(tree):
+    result = '{'
+    for key, value in tree.items():
+        if value['status'] == 'removed':
+            result += (
+                f'\n  - {key} : {value['value']}'
+            )
+        elif value['status'] == 'unchanged':
+            result += (
+                f'\n    {key} : {value['value']}'
+            )
+        elif value['status'] == 'changed':
+            result += (
+                f'\n  - {key} : {value['old_value']}'
+            )
+            result += (
+                f'\n  + {key} : {value['new_value']}'
+            )
+        elif value['status'] == 'added':
+            result += (
+                f'\n  + {key} : {value['value']}'
+            )
+    result += '\n}'
+    return result
