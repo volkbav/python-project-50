888e76863e9c43c276424fcce8bb19898c206361
volkbav <berezov86@gmail.com>
2025-05-14 16:29:40 +0300

    update generate_diff
    add make_diff func

--- gendiff/core/generate_diff.py
+++ gendiff/core/generate_diff.py
@@ -3,5 +3,38 @@ from gendiff.core.parse import parse_json
 def generate_diff(file_path1, file_path2, format=None):
     first_file_content = parse_json(file_path1)
     second_file_content = parse_json(file_path2)
-    return first_file_content, second_file_content
+    diff = make_diff(first_file_content, second_file_content)
+    
+    return first_file_content, second_file_content, diff
 
+
+def make_diff(file_data1, file_data2):
+    diff = {}
+    keys1 = set(file_data1.keys())
+    keys2 = set(file_data2.keys())
+    all_keys = sorted(keys1.union(keys2))
+    for key in all_keys:
+        if key in keys1 and key not in keys2:
+            diff[key] = {
+                'status': 'removed',
+                'value': file_data1[key]
+            }
+        elif key in keys1 and key in keys2:
+            if file_data1[key] == file_data2[key]:
+                diff[key] = {
+                    'status': 'unchanged',
+                    'value': file_data1[key]
+                    }
+            else:
+                diff[key] = {
+                    'status': 'changed',
+                    'old_value': file_data1[key],
+                    'new_value': file_data2[key]
+                }
+        elif key not in keys1 and key in keys2:
+            diff[key] = {
+                'status': 'added',
+                'value': file_data2[key]
+            }
+    return diff
+            
