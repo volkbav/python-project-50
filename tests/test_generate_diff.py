10b22a71d7d307d225b78633e98fe9a244f28ed6
volkbav <berezov86@gmail.com>
2025-05-14 17:13:42 +0300

    update tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,24 +1,13 @@
 from gendiff import generate_diff
 
-def test_step3():
-    first_file_content, second_file_content, _ = generate_diff(
-        'tests/fixtures/file1.json',
-        'tests/fixtures/file2.json'
-        )
-    print(
-        f'\n---test_step3---\n'
-        f'file1: \n {first_file_content}\n',
-        f'file2: \n {second_file_content}\n',
-        '---test_step3---\n'
-          )
-
-def test_step4():
-    _, _, diff = generate_diff(
-        'tests/fixtures/file1.json',
-        'tests/fixtures/file2.json'
-        )
-    print(
-        '\n---test_step4---\n',
-        f'diff: \n {diff}\n',
-        '---test_step4---\n'
-        )
+def test_generate_diff():
+    file_path1 = 'tests/fixtures/file1.json'
+    file_path2 = 'tests/fixtures/file2.json'
+    with open('tests/fixtures/expected1_2.txt', 'r') as file:
+        expected_result = file.read()
+        print(f'\n{expected_result}')
+    
+    result = generate_diff(file_path1, file_path2)
+    print(result)
+#    assert result == expected_result
+    
