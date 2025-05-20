58c6f21717fd29f0cacb60d444503a7c33031e1b
volkbav <berezov86@gmail.com>
2025-05-20 22:13:40 +0300

    add test

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,17 +1,38 @@
 import os
 
-from gendiff import generate_diff
+import pytest
+
+from gendiff.core.generate_diff import generate_diff
 
 
-def test_generate_diff():
+@pytest.mark.parametrize(
+    'file1, file2, expected',
+    [
+        (  # test json
+            'test_data/file1.json',
+            'test_data/file1.json',
+            'test_data/expected_flat.txt'    
+        ),
+        (  # test yml
+            'test_data/file1.yml',
+            'test_data/file1.yml',
+            'test_data/expected_flat.txt'    
+        ),
+        (  # test yaml
+            'test_data/file1.yaml',
+            'test_data/file1.yaml',
+            'test_data/expected_flat.txt'    
+        )
+    ]
+)
+def test_generate_diff(file1, file2, expected):
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path1 = os.path.join(current_dir, 'test_data', 'file1.json')
-    file_path2 = os.path.join(current_dir, 'test_data', 'file2.json')
-    with open(os.path.join(
-        current_dir, 'test_data', 'expected_flat.txt'), 'r'
-        ) as file:
-        expected_result = file.read()
-    result = generate_diff(file_path1, file_path2)
+    path1 = os.path.join(current_dir, file1)
+    path2 = os.path.join(current_dir, file2)
+    expected_path = os.path.join(current_dir, expected)
+    with open(expected_path, 'r') as file:
+        expected_result = file.read().strip()
+    result = generate_diff(path1, path2).strip()
 
     assert result == expected_result
     
