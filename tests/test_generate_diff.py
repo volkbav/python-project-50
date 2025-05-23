959c96b7a4a1e272c2737fab0f8f1d331e2d81d2
volkbav <berezov86@gmail.com>
2025-05-23 13:48:26 +0300

    add test for plain format

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -33,7 +33,7 @@ from gendiff.core.generate_diff import generate_diff
             'test_data/no_change.txt',
             'stylish' 
             ),
-        (  # stylish json
+        (  # --stylish json
             'test_data/file1.json',
             'test_data/file2.json',
             'test_data/expected_stylish.txt',
@@ -51,6 +51,24 @@ from gendiff.core.generate_diff import generate_diff
             'test_data/expected_stylish.txt',
             'stylish' 
             ),
+        (  # --plain json
+            'test_data/file1.json',
+            'test_data/file2.json',
+            'test_data/expected_plain.txt',
+            'stylish' 
+            ), 
+        (  # plain yml
+            'test_data/file1.yml',
+            'test_data/file2.yml',
+            'test_data/expected_plain.txt',
+            'stylish' 
+            ),
+        (  # plain yaml
+            'test_data/file1.yaml',
+            'test_data/file2.yaml',
+            'test_data/expected_plain.txt',
+            'stylish' 
+            ),            
     ]
 )
 def test_generate_diff(file1, file2, expected, format_name):
